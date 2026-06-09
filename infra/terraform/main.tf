cb1e21b76403ed645b13baa3ffecbfaa0aab0652
Rafael Ramalhete <rafael.ramalhete2004rgmail.com>
2026-06-09 21:15:22 +0100

    Adicionar Docker CI pipeline

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -88,6 +88,7 @@ resource "aws_instance" "app" {
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
+  key_name               = "cloud-project-key"
 
   tags = {
     Name = "cloud-project-app"
